f5f5a6bdac549cf46ded6a97c9affa919e719a14
HamzaNabil <hamzanabil913@gmail.com>
2022-03-14 21:14:57 +0200

    add Session 1 JS

---
+++ Session_1/README.md
@@ -0,0 +1,25 @@
+# JavaScript Part 1
+
+# Content :
+
+- Introduction
+
+- Where To Write
+
+- Output [ document write | getElement | console log]
+
+- Statement
+
+- Comments
+
+- Variable
+
+- Operators
+
+- Decrement | Increment
+
+# ------------------------ > Next Lec
+
+- Task: Simple Todo app
+
+- Function
---
+++ Session_1/js/main.js
@@ -0,0 +1,81 @@
+// Output
+
+// document.write(<h1> Hello Js  </h1>);
+
+// document.getElementById("demo").innerHTML = "test";
+
+// console.log("test js"); // js
+
+// Divide and conquer
+
+// 500
+
+// 250
+
+console.log("jkqwkqw"); // Test code
+
+// 250
+
+// 125
+
+// use console to test code [note]
+
+/*
+    wef
+    wef
+    wefw
+    nkwneflwefm
+*/
+
+console.log("qqq");
+
+// 125
+
+// Comments
+
+// Variables
+var myButton = document.getElementById("btn");
+
+myButton.onclick = function () {
+  myButton.innerHTML = "JS";
+};
+
+// Data Types
+
+var x = 1; // number
+var s = "hamza"; // string
+var b = false; // boolean
+
+// var num = 123
+
+// num
+
+// num
+
+// num
+
+// Operators + - * / % ++ --
+// ++ increment
+// -- decrement
+
+var o = 1;
+var n = 5;
+
+var sum = n % o; // 6 % 2  = 0  => even  // 7 % 2 odd
+console.log(sum);
+
+var myNumber = 2;
+
+myNumber++; // 3
+++myNumber; // 4
+
+myNumber--; // 3
+++myNumber; // 4
+
+console.log(myNumber); // 4
+console.log(myNumber++); // 4
+// 5
+myNumber--; // 4
+--myNumber; // 3
+
+console.log(--myNumber); // 2
